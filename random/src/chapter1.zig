439810b89eceb1c523a73d78c9f47bd96abd8e3a
ash <ashraja941@gmail.com>
2025-09-09 13:39:28 -0500

    created chapter files for random code

---
+++ random/src/chapter1.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn arrayOperations() void {
+    std.debug.print("Array Operations\n", .{});
+
+    const a = [_]u8{ 1, 2, 3 };
+    const b = [_]u8{ 4, 5 };
+    std.debug.print("a : {any}\nb : {any}\n", .{ a, b });
+
+    const c = a ++ b;
+    std.debug.print("c : {any}\n", .{c});
+
+    const d = c ++ c;
+    std.debug.print("d : {any}\n", .{d});
+}
+
+pub fn breakToPoint() void {
+    var y: i32 = 123;
+    const x = add_one: {
+        y += 1;
+        break :add_one y;
+    };
+    if (x == 124 and y == 124) {
+        std.debug.print("Hey!", .{});
+    }
+}
