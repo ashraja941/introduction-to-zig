de27ffe2e05ff53eb5a5d199f8442f171f41b2e4
ash <ashraja941@gmail.com>
2025-09-16 11:25:31 -0500

    updated until 3.3.9

--- random/src/chapter3.zig
+++ random/src/chapter3.zig
@@ -2,7 +2,60 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    try createString();
+    try arenaAllocatorTest();
+}
+
+pub fn initateArray() void {
+    var buffer: [10]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    print("{any}", .{buffer});
+
+    const buffer2: [10]u8 = [1]u8{0} ** 10;
+    print("{any}", .{buffer2});
+}
+pub fn arenaAllocatorTest() !void {
+    // arena allocator needs some overhead to make sure that it tracks
+    var buffer: [100]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    var aa = std.heap.ArenaAllocator.init(fba.allocator());
+    const allocator = aa.allocator();
+    defer aa.deinit();
+
+    const input = try allocator.alloc(u8, 2);
+    // defer allocator.free(input);
+
+    const input2 = try allocator.alloc(u8, 2);
+    // defer allocator.free(input2);
+
+    print("{any}", .{input});
+    print("{any}", .{input2});
+}
+pub fn stackFixedBuffer() !void {
+    var buffer: [10]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const input = try allocator.alloc(u8, 5);
+    defer allocator.free(input);
+
+    const input2 = try allocator.alloc(u8, 5);
+    defer allocator.free(input2);
+
+    // const input3 = try allocator.alloc(u8, 5);
+    // defer allocator.free(input3);
+
+    print("{any}", .{input});
+    // print("{any}", .{input2});
+    // print("{any}", .{input3});
 }
 
 pub fn createString() !void {
