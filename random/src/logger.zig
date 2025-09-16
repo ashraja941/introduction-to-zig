d1a568dd240e3c77b32c41f61112171b2f5dd5f9
ash <ashraja941@gmail.com>
2025-09-16 12:42:03 -0500

    added logging

---
+++ random/src/logger.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub const options: std.Options = .{
+    .log_level = .info,
+    .logFn = logger,
+};
+
+pub fn logger(comptime level: std.log.Level, comptime scope: @Type(.enum_literal), comptime format: []const u8, args: anytype) void {
+    // Ignore all non-error logging from sources other than
+    // .my_project, .nice_library and the default
+    const scope_prefix = "(" ++ switch (scope) {
+        .my_project, .nice_library, std.log.default_log_scope => @tagName(scope),
+        else => if (@intFromEnum(level) <= @intFromEnum(std.log.Level.err))
+            @tagName(scope)
+        else
+            return,
+    } ++ "): ";
+
+    const prefix = "[" ++ comptime level.asText() ++ "] " ++ scope_prefix;
+
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print("\n" ++ prefix ++ format ++ "\n", args) catch return;
+}
--- random/src/main.zig
+++ random/src/main.zig
@@ -3,8 +3,14 @@ const c1 = @import("chapter1.zig");
 const c2 = @import("chapter2.zig");
 const c3 = @import("chapter3.zig");
 
+pub const std_options = @import("logger.zig").options;
+
 pub fn main() !void {
+    std.log.info("Running...", .{});
+
     // try c1.nonEnglish();
     // c2.main();
     try c3.main();
+
+    std.log.info("Finished Execution...", .{});
 }
