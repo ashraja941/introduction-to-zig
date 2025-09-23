7c34305d4875392cd3fccb02e27b2ed07d6b1a80
ash <ashraja941@gmail.com>
2025-09-22 23:19:06 -0500

    finished project 2

--- HTTPServer/src/main.zig
+++ HTTPServer/src/main.zig
@@ -1,11 +1,37 @@
 const std = @import("std");
+const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
+const Method = Request.Method;
+const Response = @import("response.zig");
 
 const std_options = @import("logging.zig").options;
 
-const stdout_file = std.io.getStdOut().writer();
-var bw = std.io.bufferedWriter(stdout_file);
-const stdout = bw.writer();
-
 pub fn main() !void {
-    std.log.debug("Starting the main function", .{});
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+    std.log.info("Starting the Server", .{});
+
+    const socket = try SocketConf.Socket.init();
+    std.log.debug("Socket address : {any}", .{socket._address});
+
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+
+    var buffer: [1000]u8 = [1]u8{0} ** 1000;
+    try Request.read_request(connection, buffer[0..]);
+
+    const request = Request.parse_request(buffer[0..]);
+
+    if (request.method == Method.GET) {
+        if (std.mem.eql(u8, request.uri, "/")) {
+            try Response.send_200(connection);
+        } else {
+            try Response.send_404(connection);
+        }
+    }
+    std.log.info("Closing the server", .{});
+
+    try stdout.print("Buffer Information : {any}\n", .{request});
+    try bw.flush();
 }
