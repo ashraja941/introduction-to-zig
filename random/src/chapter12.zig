306ec0c216dfd20b5b0ba74ffce7dba7bb4b5269
ash <ashraja941@gmail.com>
2025-09-25 23:11:14 -0500

    updated until 14

--- random/src/chapter12.zig
+++ random/src/chapter12.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 pub fn main() !void {
-    try currentWorkingDirectory();
+    try readSomeFile();
 }
 
 fn readAndWrite() !void {
@@ -32,4 +32,29 @@ fn currentWorkingDirectory() !void {
     const fww = &fw.interface;
     _ = try fww.writeAll("Writing this line to the file\n");
     try fww.flush();
+}
+
+fn readSomeFile() !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+    const cwd = std.fs.cwd();
+    const file = try cwd.createFile("foo.txt", .{ .read = true });
+    defer file.close();
+
+    var file_buffer: [1024]u8 = undefined;
+    var fww = file.writer(&file_buffer);
+    const fw = &fww.interface;
+    _ = try fw.writeAll("We are going to read this line\n");
+    try fw.flush();
+
+    var buffer: [300]u8 = undefined;
+    @memset(buffer[0..], 0);
+    try file.seekTo(0);
+    var read_buffer: [1024]u8 = undefined;
+    var frr = file.reader(&read_buffer);
+    const fr = &frr.interface;
+    _ = try fr.readSliceShort(buffer[0..]);
+    try stdout.print("Read this : {s}\n", .{buffer});
+    try stdout.flush();
 }
